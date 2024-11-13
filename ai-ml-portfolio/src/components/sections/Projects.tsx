b1342ff4c084392a9880028eb000a52792041b5d
ViratSrivastava <vs.viratsrivastava11111@gmail.com>
2024-11-13 22:16:37 +0530

    Create Projects.tsx

---
+++ ai-ml-portfolio/src/components/sections/Projects.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const PageName: React.FC = () => {
+  return (
+    <div className="Project">
+      <h1>Page Title</h1>
+      {/* Page content */}
+    </div>
+  )
+}
+
+export default PageName
