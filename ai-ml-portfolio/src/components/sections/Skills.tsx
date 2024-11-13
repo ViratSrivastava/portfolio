2becb40fb1d8d3cdf9dc41bfe306e3ed7b75920e
ViratSrivastava <vs.viratsrivastava11111@gmail.com>
2024-11-13 22:16:41 +0530

    Create Skills.tsx

---
+++ ai-ml-portfolio/src/components/sections/Skills.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const PageName: React.FC = () => {
+  return (
+    <div className="Skills">
+      <h1>Page Title</h1>
+      {/* Page content */}
+    </div>
+  )
+}
+
+export default PageName
