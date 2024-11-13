e2ccb28472a38792f11de768e3f9cd9909703c7c
ViratSrivastava <vs.viratsrivastava11111@gmail.com>
2024-11-13 22:16:29 +0530

    Create Experience.tsx

---
+++ ai-ml-portfolio/src/components/sections/Experience.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const PageName: React.FC = () => {
+  return (
+    <div className="Experience">
+      <h1>Page Title</h1>
+      {/* Page content */}
+    </div>
+  )
+}
+
+export default PageName
